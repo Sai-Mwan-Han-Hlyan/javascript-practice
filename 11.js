72507f3178dc99bdd2f79735f5ce0516ea3955bd
sai mwan han hlyan <saimwanhanhlyan@gmail.com>
2025-10-04 06:51:12 +0700

    day four

---
+++ 11.js
@@ -0,0 +1,59 @@
+// const nums = [10, 20, 30];
+
+// nums[2] = 99;
+
+// console.log(nums);
+
+// for (let i = 0; i < 11; i += 2) {
+//   console.log(i);
+// }
+
+// for (let i = 5; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   i += 2;
+//   console.log(i);
+// }
+
+// function addNum(array, num) {
+//   let result = [];
+//   for (let i = 0; i < array.length; i++) {
+//     result.push(array[i] + num);
+//   }
+//   return result;
+// }
+
+// console.log(addNum([1, 2, 3], 2));
+
+// function addArrays(array1, array2) {
+//   const total = [];
+
+//   for (let i = 0; i < array1.length; i++) {
+//     total.push(array1[i] + array2[i]);
+//   }
+//   return total;
+// }
+
+// console.log(addArrays([1, 2, 3], [1, 1, 3]));
+
+// function countPositive(nums) {
+//   let result = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > 0) {
+//       result++;
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(countPositive([1, -3, 5]));
+// console.log(countPositive([-2, 3, -5, 7, 10]));
+
+function minMax(nums) {
+  for (let i = 0; i < nums.length; ) {}
+}
--- index.js
+++
@@ -1,5 +0,0 @@
-if (13 >= 16) {
-  console.log("you can drive");
-} else {
-  console.log("you cant ddrive");
-}
